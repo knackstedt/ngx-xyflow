ea46e1923efcf9a418bd5ef1636f98819caeb1dd
Andrew G. Knackstedt <knackstedt@outlook.com>
2024-12-14 22:23:05 -0500

    fix: correct build bundle with public-api file

---
+++ src/xyflow/public-api.ts
@@ -0,0 +1,10 @@
+export * from './xyflow.module';
+export * from './xyflow.component';
+
+export * from './background.directive';
+export * from './controls.directive';
+export * from './handle.directive';
+export * from './minimap.directive';
+export * from './node-resizer.directive';
+export * from './node-toolbar.directive';
+export * from './node.directive';
