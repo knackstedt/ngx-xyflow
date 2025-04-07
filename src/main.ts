cbbefc629867837a7f2dcc715731cbfb10616442
Andrew G. Knackstedt <knackstedt@outlook.com>
2025-04-06 21:52:49 -0400

    fix: upgrade to ngx-reactify@0.1.0 to resolve #11.

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,14 @@
 import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 import { importProvidersFrom } from '@angular/core';
 import { BrowserModule, bootstrapApplication } from '@angular/platform-browser';
-import { provideAnimations } from '@angular/platform-browser/animations';
+// import { provideAnimations } from '@angular/platform-browser/animations';
 import 'zone.js'; // Included with Angular CLI.
 import { AppComponent } from './app/app.component';
 
 bootstrapApplication(AppComponent, {
     providers: [
         importProvidersFrom(BrowserModule),
-        provideAnimations(),
+        // provideAnimations(),
         provideHttpClient(withInterceptorsFromDi())
     ]
 })
--- src/xyflow/node.directive.ts
+++ src/xyflow/node.directive.ts
@@ -16,13 +16,13 @@ import {
     TemplateRef,
     ViewChildren
 } from '@angular/core';
-import { ReactifyNgComponent, ReactifyAngularComponent } from 'ngx-reactify';
 import { Handle, NodeResizer, NodeToolbar } from '@xyflow/react';
 import * as React from 'react';
 import { HandleDirective } from './handle.directive';
 import { XYFlowComponent } from './xyflow.component';
 import { NodeResizerDirective } from './node-resizer.directive';
 import { NodeToolbarDirective } from './node-toolbar.directive';
+import { AutoWrapAngularObject, ng2ReactProps } from 'ngx-reactify';
 
 
 @Component({
@@ -121,77 +121,28 @@ export class NodeDirective {
             return;
         }
 
-        const getProps = (obj = {}) => {
-            const props = {};
-            Object.entries(obj).forEach(([k, v]) => {
-                // Omit things prefixed with an underscore
-                if (k.startsWith('_')) return;
-                // Omit output event emitters
-                if (k.startsWith('on')) return;
-
-                props[k] = v;
-            });
-            return props;
-        }
-
-        this.xyflow.nodeTypes[this.nodeType] = ReactifyAngularComponent({
+        this.xyflow.nodeTypes[this.nodeType] = AutoWrapAngularObject({
             component: XYFlowNodeComponent,
             appRef: this.appRef,
             injector: this.injector,
+            instance: this,
             ngZone: this.ngZone,
+            containerTag: "ngx-xyflow-node-container",
+            reactTemplate: el => {
+                return React.createElement(React.Fragment, {},
+                    ...[
+                        this.nodeResizer ? React.createElement(NodeResizer, ng2ReactProps(this.nodeResizer)) : null,
+                        this.nodeToolbar ? React.createElement(NodeToolbar, ng2ReactProps(this.nodeToolbar)) : null,
+                        el,
+                        ...(this.handles?.map(handle => {
+                            const props = ng2ReactProps(handle) as any;
+                            // props.onConnect = (connection) =>
+                            //     handle.onConnect.next(connection);
 
-            preSiblings: [
-                this.nodeResizer ? React.createElement(NodeResizer, getProps(this.nodeResizer)) : null,
-                this.nodeToolbar ? React.createElement(NodeToolbar, getProps(this.nodeToolbar)) : null
-            ].filter(s => s),
-            additionalChildren: [
-                ...(this.handles?.map(handle => {
-                    const props = getProps(handle) as any;
-                    // props.onConnect = (connection) =>
-                    //     handle.onConnect.next(connection);
-
-                    return React.createElement(Handle, props);
-                }) || []),
-            ].filter(c => c),
-
-            rootElementName: React.Fragment as any,// "ngx-xyflow-node",
-            containerElementName: "ngx-xyflow-node-container",
-            staticInputs: { template: this.template },
-            staticOutputs: {
-                blur: (e) => this.blur.emit(e),
-                click: (e) => this.click.emit(e),
-                contextMenu: (e) => this.contextmenu.emit(e),
-                dblclick: (e) => this.dblclick.emit(e),
-                drag: (e) => this.drag.emit(e),
-                dragend: (e) => this.dragend.emit(e),
-                dragenter: (e) => this.dragenter.emit(e),
-                dragleave: (e) => this.dragleave.emit(e),
-                dragover: (e) => this.dragover.emit(e),
-                dragstart: (e) => this.dragstart.emit(e),
-                focus: (e) => this.focus.emit(e),
-                keydown: (e) => this.keydown.emit(e),
-                keyup: (e) => this.keyup.emit(e),
-                keypress: (e) => this.keypress.emit(e),
-                mousedown: (e) => this.mousedown.emit(e),
-                mouseenter: (e) => this.mouseenter.emit(e),
-                mouseleave: (e) => this.mouseleave.emit(e),
-                mousemove: (e) => this.mousemove.emit(e),
-                mouseout: (e) => this.mouseout.emit(e),
-                mouseover: (e) => this.mouseover.emit(e),
-                mouseup: (e) => this.mouseup.emit(e),
-                mousewheel: (e) => this.mousewheel.emit(e),
-                pointercancel: (e) => this.pointercancel.emit(e),
-                pointerdown: (e) => this.pointerdown.emit(e),
-                pointerenter: (e) => this.pointerenter.emit(e),
-                pointerleave: (e) => this.pointerleave.emit(e),
-                pointermove: (e) => this.pointermove.emit(e),
-                pointerout: (e) => this.pointerout.emit(e),
-                pointerover: (e) => this.pointerover.emit(e),
-                pointerrawupdate: (e) => this.pointerrawupdate.emit(e),
-                pointerup: (e) => this.pointerup.emit(e),
-                resize: (e) => this.resize.emit(e),
-                scroll: (e) => this.scroll.emit(e),
-                wheel: (e) => this.wheel.emit(e),
+                            return React.createElement(Handle, props);
+                        }) || []),
+                    ].filter(n => !!n)
+                )
             }
         })
     }
